e2ed5aae7b8dc207729976b08ac1ac277070b521
Rafiq <rafiq@thydreamtech.com>
2017-03-29 21:36:55 +0530

    Css Tweakes

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+
+  def date_customizer(date)
+    return "Today" if date == Date.today.strftime("%m/%d/%Y")
+    return "Yesterday" if date == Date.yesterday.strftime("%m/%d/%Y")
+    return date
+  end
 end
+
